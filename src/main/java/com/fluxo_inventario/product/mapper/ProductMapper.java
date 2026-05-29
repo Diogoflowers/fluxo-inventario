10ad137b2c122e6310226140bf205b2c5508c5a6
Diogo Flowers <diogoflores2009@gmail.com>
2026-05-28 22:41:02 -0300

    refactor: extract product mapper

--- src/main/java/com/fluxo_inventario/product/mapper/ProductMapper.java
+++ src/main/java/com/fluxo_inventario/product/mapper/ProductMapper.java
@@ -1,5 +1,30 @@
 package com.fluxo_inventario.product.mapper;
 
+import com.fluxo_inventario.product.dto.ProductRequestDTO;
+import com.fluxo_inventario.product.dto.ProductResponseDTO;
+import com.fluxo_inventario.product.entity.Product;
+
 public class ProductMapper {
 
+    public static Product toEntity(ProductRequestDTO dto) {
+
+        Product product = new Product();
+
+        product.setName(dto.name());
+        product.setDescription(dto.description());
+        product.setPrice(dto.price());
+        product.setQuantity(dto.quantity());
+
+        return product;
+    }
+
+    public static ProductResponseDTO toResponseDTO(Product product) {
+
+        return new ProductResponseDTO(
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getQuantity()
+        );
+    }
 }
--- src/main/java/com/fluxo_inventario/product/service/ProductService.java
+++ src/main/java/com/fluxo_inventario/product/service/ProductService.java
@@ -3,6 +3,7 @@ package com.fluxo_inventario.product.service;
 import com.fluxo_inventario.product.dto.ProductRequestDTO;
 import com.fluxo_inventario.product.dto.ProductResponseDTO;
 import com.fluxo_inventario.product.entity.Product;
+import com.fluxo_inventario.product.mapper.ProductMapper;
 import com.fluxo_inventario.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -19,33 +20,18 @@ public class ProductService {
 
     public ProductResponseDTO createProduct(ProductRequestDTO dto) {
 
-        Product product = new Product();
-
-        product.setName(dto.name());
-        product.setDescription(dto.description());
-        product.setPrice(dto.price());
-        product.setQuantity(dto.quantity());
+        Product product = ProductMapper.toEntity(dto);
 
         Product savedProduct = productRepository.save(product);
 
-        return new ProductResponseDTO(
-                savedProduct.getName(),
-                savedProduct.getDescription(),
-                savedProduct.getPrice(),
-                savedProduct.getQuantity()
-        );
+        return ProductMapper.toResponseDTO(savedProduct);
     }
 
     public List<ProductResponseDTO> getAllProducts() {
 
         return productRepository.findAll()
                 .stream()
-                .map(product -> new ProductResponseDTO(
-                        product.getName(),
-                        product.getDescription(),
-                        product.getPrice(),
-                        product.getQuantity()
-                ))
+                .map(ProductMapper::toResponseDTO)
                 .toList();
     }
 }
